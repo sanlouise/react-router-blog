747bef2d8a806b7d67a98d21e59ca9b40e432a7f
sanlouise <sandra.adamshallie@gmail.com>
2017-08-28 18:03:15 +0200

    Add ShowPost component

--- src/components/ShowPost/index.js
+++ src/components/ShowPost/index.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class ShowPost extends Component {
-  render () {
-    return <div className='ShowPost'>
-      ...
-    </div>
+  constructor(props) {
+    super(props)
+    this.state = {
+      author: "",
+      title: "",
+      blogPost: "",
+    }
+  }
+
+  componentDidMount() {
+    console.log("Hey I mounted!");
+    const { id } = this.props.match.params;
+    console.log(id);
+    const URL = `https://tiny-lasagna-server.herokuapp.com/collections/blogger/${id}`;
+
+    fetch(URL).then(results => {
+      return results.json();
+    }).then(res => {
+      this.setState({
+        author: res.author,
+        title: res.title,
+        blogPost: res.blogPost,
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div className="">
+        <h1>{this.state.title}</h1>
+        <p className="author">{this.state.author}</p>
+        <p>{this.state.blogPost}</p>
+      </div>
+    )
   }
 }
 
-export default ShowPost
+export default ShowPost;
