a5374ce20095bcbd5bd1ad6e4d0ed2cdc4cf79a5
sanlouise <sandra.adamshallie@gmail.com>
2017-08-28 18:04:43 +0200

    Add List component

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class PostList extends Component {
-  render () {
-    return <div className='PostList'>
-      ...
-    </div>
+export default class PostList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/')
+    .then(res => {
+      return res.json();
+    })
+    .then(posts => {
+      this.setState({ posts });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map((post) => {
+          const url = `/posts/${post._id}`
+          return(
+            <div className="post" key={post._id}>
+              <h1><Link to={url}>{post.title}</Link></h1>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 }
-
-export default PostList
